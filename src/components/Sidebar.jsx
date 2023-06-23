3251646ccf5954c68ec42d2b0c4c9d4ddecb7677
unbearabletruth <antaarty@gmail.com>
2023-06-23 13:44:09 +0700

    all pages into main block

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -5,31 +5,28 @@ import logo from '../assets/images/twitter-icon.svg'
 function Sidebar() {
   return(
     <div id="sidebar">
-      <img 
-        src={logo} 
-        alt='logo'
-        id='sidebarLogo'
-      >
-      </img>
-      <div className="sidebarLinks">
-        <Link to="/" className="sidebarLink">Home</Link>
-        <Link to="/explore" className="sidebarLink">Explore</Link>
-        <Link to="/notifications" className="sidebarLink">Notifications</Link>
-        <Link to="/messages" className="sidebarLink">Messages</Link>
-        <Link to="/lists" className="sidebarLink">Lists</Link>
-        <Link to="/bookmarks" className="sidebarLink">Bookmarks</Link>
-        <Link to="/verified" className="sidebarLink">Verified</Link>
-        <Link to="/profile" className="sidebarLink">Profile</Link>
-        <div className="sidebarLink">More</div>
-        <Link to="/compose" id='composeTweet'></Link>
-      </div>
-      <div className="sidebarUserInfo">
-        <img></img>
-        <div>
-          <p>Name</p>
-          <p>profile name</p>
+      <div id='sidebarContent'>
+        <div className="sidebarLinks">
+          <img src={logo} alt='logo' id='sidebarLogo'></img>
+          <Link to="/" className="sidebarLink">Home</Link>
+          <Link to="/explore" className="sidebarLink">Explore</Link>
+          <Link to="/notifications" className="sidebarLink">Notifications</Link>
+          <Link to="/messages" className="sidebarLink">Messages</Link>
+          <Link to="/lists" className="sidebarLink">Lists</Link>
+          <Link to="/bookmarks" className="sidebarLink">Bookmarks</Link>
+          <Link to="/verified" className="sidebarLink">Verified</Link>
+          <Link to="/profile" className="sidebarLink">Profile</Link>
+          <div className="sidebarLink">More</div>
+          <Link to="/compose" id='composeTweet'></Link>
         </div>
-        <img></img>
+        <div className="sidebarUserInfo">
+          <img></img>
+          <div>
+            <p>Name</p>
+            <p>profile name</p>
+          </div>
+          <img></img>
+        </div>
       </div>
     </div>
   )
